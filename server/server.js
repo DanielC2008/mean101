796a68476b90d795b1e3e6928dac16347a05a5dd
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-09-26 10:56:51 -0500

    title sent from node and styled w/ bootstrap

--- server/server.js
+++ server/server.js
@@ -7,4 +7,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('client'))
 
+app.get('/api/title', (req,res) => {
+	res.json({ title: 'Mean 101 from Node'})
+})
+
 app.listen(port, ()=> console.log(`Listening on port: ${port}`))
