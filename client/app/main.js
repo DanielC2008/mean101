bf9cf56f03dd444f2a7b74f4bc97566a83f7444e
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-09-26 12:30:40 -0500

    chat room get and post requests

--- client/app/main.js
+++ client/app/main.js
@@ -1,7 +1,36 @@
 'use strict'
 
 angular
-	.module('mean101', [])
+	.module('mean101', ['ngRoute'])
+	.config($routeProvider =>
+		$routeProvider
+			.when('/', {
+				controller: 'main',
+				templateUrl: 'partials/main.html'
+			})
+			.when('/chat', {
+				controller: 'ChatCtrl',
+				templateUrl: 'partials/chat.html'
+			})
+	)
 	.controller('main', function($scope, $http) {
-		$http.get('/api/title').then(({data: {title}}) => $scope.title = title)
+		$http.get('/api/title')
+			.then(({data: {title}}) =>
+			$scope.title = title
+		)
+	})
+	.controller('ChatCtrl', function($scope, $http) {
+		$scope.sendMessage = () => {
+			const msg = {
+				author: $scope.author,
+				content: $scope.content
+			}
+			$http.post('api/messages', msg)
+			.then(() => $scope.messages.push(msg))
+		}
+
+		$http.get('/api/messages')
+			.then(({data: {messages}}) => {
+				$scope.messages = messages
+			})
 	})
